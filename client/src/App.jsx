0ec222665a03d33a73389896a2ee1ec222a26151
Mideny21 <30451596+Mideny21@users.noreply.github.com>
2021-09-01 18:01:58 +0300

    norma changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,24 +3,38 @@ import Home from "./pages/home/Home";
 import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
 import Login from "./pages/login/Login";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 const App = () => {
+  const user = true;
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          <Home />
+          {user ? <Home /> : <Redirect to="/register" />}
         </Route>
-        <Route path="/movies">
-          <Home type="movies" />
+        <Route path="/register">
+          {!user ? <Register /> : <Redirect to="/" />}
         </Route>
-        <Route path="/series">
-          <Home type="series" />
-        </Route>
-        <Route path="/watch">
-          <Watch />
-        </Route>
+        <Route path="/login">{!user ? <Login /> : <Redirect to="/" />}</Route>
+        {user && (
+          <>
+            <Route path="/movies">
+              <Home type="movies" />
+            </Route>
+            <Route path="/series">
+              <Home type="series" />
+            </Route>
+            <Route path="/watch">
+              <Watch />
+            </Route>
+          </>
+        )}
       </Switch>
     </Router>
   );
